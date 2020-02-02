1bb77e3ddda8741d1f994d921679b8fcba334d66
Mark Thomas Miller <mark@mayvalellc.com>
2020-02-02 12:34:29 -0500

    Draft basic Gallery component

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Gallery.module.css";
+import NavigationArrow from "../NavigationArrow";
 
-function Gallery({ images }) {
+export default function Gallery({ images }) {
+  const [current, setCurrent] = useState(0);
+  const image = images[current];
+
+  function increment(n) {
+    const lastIndex = images.length - 1;
+    const next = current + n;
+    if (next > lastIndex) setCurrent(0);
+    else if (next < 0) setCurrent(lastIndex);
+    else setCurrent(next);
+  }
+
   return (
     <div className={styles.gallery}>
-      <img src="/assets/example.png" alt="TODO" />
+      <img src={image.url} alt={image.current} />
+      <NavigationArrow direction="left" onClick={() => increment(-1)} />
+      <NavigationArrow direction="right" onClick={() => increment(1)} />
     </div>
   );
 }
-
-export default Gallery;
