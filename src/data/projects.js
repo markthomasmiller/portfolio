986cd4f5a2d581c4df2b8cbb3afc13741b15971f
Mark Thomas Miller <mark@mayvalellc.com>
2020-02-01 19:27:24 -0500

    Add sample project data

---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    title: "projectname",
+    description: "projectdescription",
+    link: {
+      text: "linktext",
+      url: "https://example.com"
+    },
+    skills: ["a", "b", "c"],
+    fact: "fact",
+    images: [
+      {
+        url: "https://example.com/cat.png",
+        description: "imagedescription"
+      },
+      {
+        url: "https://example.com/cat.png",
+        description: "imagedescription"
+      },
+      {
+        url: "https://example.com/cat.png",
+        description: "imagedescription"
+      }
+    ]
+  }
+];
