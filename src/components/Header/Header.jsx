cef1bbc4ec79eb8050b5ed95311f0c2923c44c9e
Mark Thomas Miller <mark@mayvalellc.com>
2020-02-02 12:32:54 -0500

    Add logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./Header.module.css";
+import logo from "../../assets/logo.svg";
 
 const Header = () => (
   <div className={styles.header}>
-    <a href="https://zeph.co">(Mark signature)</a>
+    <a href="https://zeph.co">
+      <img src={logo} alt="logo" />
+    </a>
     <a href="mailto:mark@mayvalellc.com">Contact</a>
   </div>
 );
