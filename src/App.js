fa600d89b5cfecbb4fff9e8bf639b1e631eb0051
Mark Thomas Miller <mark@mayvalellc.com>
2020-02-01 19:27:50 -0500

    Add basic components to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
 
-function App() {
-  return <div>Placeholder</div>;
-}
+const App = () => (
+  <div>
+    <Header />
+    <Projects />
+    <CTA />
+    <Footer />
+  </div>
+);
 
 export default App;
