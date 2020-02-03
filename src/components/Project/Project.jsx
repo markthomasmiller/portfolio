0d1a668068aeef7ae366ddf59318b9090051dcd5
Mark Thomas Miller <mark@mayvalellc.com>
2020-02-03 11:47:18 -0500

    Add Markdown support to project descriptions

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Project.module.css";
 import Gallery from "../Gallery";
 import Skills from "../Skills";
+import Markdown from "react-markdown";
 
 const Project = ({ project }) => {
   const { images, title, description, link, skills } = project;
@@ -10,9 +11,13 @@ const Project = ({ project }) => {
       <Gallery images={images} />
       <div className={styles.columns}>
         <div className={styles.left}>
-          <h2>{title}</h2>
-          <p>{description}</p>
-          <a href={link.url}>{link.text} →</a>
+          <h3>{title}</h3>
+          <Markdown source={description} />
+          {link ? (
+            <a href={link.url}>{link.text} →</a>
+          ) : (
+            <div className={styles.comingSoon}>Link coming soon</div>
+          )}
         </div>
         <div className={styles.right}>
           <Skills skills={skills} />
