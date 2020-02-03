7d4771dba4ff7c8d32cd33e4fb0f33155cad354f
Mark Thomas Miller <mark@mayvalellc.com>
2020-02-03 11:47:27 -0500

    Add real project descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,9 +2,9 @@ export default [
   {
     title: "MageTools",
     description:
-      "I was the sole engineer and designer for MageTools, a faster alternative to Google Chrome's DevTools inspector. I handled everything from designing its branding and onboarding to engineering its main features.",
+      "I was the _sole engineer and designer_ for MageTools, a faster alternative to Google Chrome's DevTools inspector. I handled everything from designing its branding and onboarding to engineering its main features.",
     link: {
-      text: "Website",
+      text: "Go to website",
       url: "https://mage.tools"
     },
     skills: [
@@ -18,131 +18,269 @@ export default [
     ],
     images: [
       {
-        url: "/assets/example2.png",
+        url: "/assets/1.png",
         description: "imagedescription"
       },
       {
-        url: "/assets/example.png",
+        url: "/assets/2.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/3.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/4.png",
         description: "imagedescription"
       }
     ]
   },
   {
     title: "Diversify Pittsburgh",
-    description: "projectdescription",
-    link: {
-      text: "linktext",
-      url: "https://example.com"
-    },
-    skills: ["a", "b", "c"],
+    description:
+      "I was hired to _design and develop this 50+ page B2B application_, from choosing the tech stack to developing the user flows and writing the algorithm that helps people discover contracting opportunities in their industry. I also created a user role management system.",
+    skills: [
+      "Ruby on Rails",
+      "JavaScript",
+      "UI",
+      "UX",
+      "User flow",
+      "Mobile design"
+    ],
     images: [
       {
-        url: "https://example.com/cat.png",
+        url: "/assets/1.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/2.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/3.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/4.png",
         description: "imagedescription"
       }
     ]
   },
   {
     title: "Pittsburgh BizGrid",
-    description: "projectdescription",
+    description:
+      "I was hired to design and develop this React application in one month. My client and I worked alongside Google and Carnegie Mellon University in brainstorming sessions. The design is responsive, and _the operating cost of the website is nothing_, other than paying for the domain name each year.",
     link: {
-      text: "linktext",
-      url: "https://example.com"
+      text: "Go to website",
+      url: "https://pghbizgrid.app"
     },
-    skills: ["a", "b", "c"],
+    skills: ["React", "JSON", "UI", "UX", "Responsive design", "JSON"],
     images: [
       {
-        url: "https://example.com/cat.png",
+        url: "/assets/1.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/2.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/3.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/4.png",
         description: "imagedescription"
       }
     ]
   },
   {
     title: "Featurebridge",
-    description: "projectdescription",
+    description:
+      "I designed and developed this application to help companies collect feature requests. It allows companies to embed a feature request portal on their sites and even pass in a user's information to allow them to vote seamlessly. I developed it with Ruby on Rails and created a customer-facing (JavaScript-based) frontend API.",
     link: {
-      text: "linktext",
-      url: "https://example.com"
+      text: "Go to website",
+      url: "https://featurebridge.com"
     },
-    skills: ["a", "b", "c"],
+    skills: ["Ruby on Rails", "JavaScript", "UI", "UX", "Branding"],
     images: [
       {
-        url: "https://example.com/cat.png",
+        url: "/assets/1.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/2.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/3.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/4.png",
         description: "imagedescription"
       }
     ]
   },
   {
     title: "Frontend at Jump",
-    description: "projectdescription",
+    description:
+      "I was hired to overhaul an application from Ruby on Rails to React in a very short timeframe. I created their component library, _developed dynamic landing pages_, and rewrote their CSS framework (resulting in 80% bandwidth savings). I _helped build the frontend for their main application, billing portal_, and parts of their marketing site.",
     link: {
-      text: "linktext",
-      url: "https://example.com"
+      text: "Go to website",
+      url: "https://playonjump.com"
     },
-    skills: ["a", "b", "c"],
+    skills: [
+      "React",
+      "Ruby on Rails",
+      "Accessibility",
+      "HTML",
+      "CSS",
+      "Electron",
+      "JSON"
+    ],
     images: [
       {
-        url: "https://example.com/cat.png",
+        url: "/assets/1.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/2.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/3.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/4.png",
         description: "imagedescription"
       }
     ]
   },
   {
     title: "Everpage",
-    description: "projectdescription",
-    link: {
-      text: "linktext",
-      url: "https://example.com"
-    },
-    skills: ["a", "b", "c"],
+    description:
+      "I designed and developed this landing page builder with React. Users could arrange their preferred sections (pricing charts, FAQ sections, feature descriptions), edit their content, and select their favorite font and color to _generate their own landing page in minutes_.",
+    skills: [
+      "React",
+      "MobX",
+      "Serverless",
+      "Browser storage APIs",
+      "HTML",
+      "CSS",
+      "UI",
+      "UX",
+      "Typography",
+      "Creative direction",
+      "Memberful API"
+    ],
     images: [
       {
-        url: "https://example.com/cat.png",
+        url: "/assets/1.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/2.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/3.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/4.png",
         description: "imagedescription"
       }
     ]
   },
   {
     title: "Sorcerer",
-    description: "projectdescription",
+    description:
+      "Developers spend between 7 to 9 hours per day looking at their text editors. I designed an open source VS Code theme, primarily for React development, that was useful for these long work days. This was primarily an _exercise in color theory for a product that people use all day, every day_. I also designed and developed its landing page.",
     link: {
-      text: "linktext",
-      url: "https://example.com"
+      text: "Go to website",
+      url: "https://sorcerer.mage.tools"
     },
-    skills: ["a", "b", "c"],
+    skills: ["UI", "UX", "Branding", "Color theory"],
     images: [
       {
-        url: "https://example.com/cat.png",
+        url: "/assets/1.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/2.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/3.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/4.png",
         description: "imagedescription"
       }
     ]
   },
   {
     title: "Diversify Pittsburgh, Admin Dashboard",
-    description: "projectdescription",
-    link: {
-      text: "linktext",
-      url: "https://example.com"
-    },
-    skills: ["a", "b", "c"],
+    description:
+      "I designed and developed this _data-heavy admin dashboard_ to help my client manage their users and _track platform metrics_. I added charts for data visualization, user roles, the functionality to approve users, and performance insights, and admin notifications.",
+    skills: ["Ruby on Rails", "JavaScript", "Chart.js", "UI", "UX"],
     images: [
       {
-        url: "https://example.com/cat.png",
+        url: "/assets/1.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/2.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/3.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/4.png",
         description: "imagedescription"
       }
     ]
   },
   {
     title: "MageTools marketing site",
-    description: "projectdescription",
+    description:
+      "I designed and developed a _media-rich marketing site_ with a live demo, promotional video, and various gifs on the homepage. I chose to build on Jekyll for performance benefits, as well as speed of development. In addition to being easy to edit, the site also _controls the user onboarding for MageTools_ and hosts the _serverless function used for validating licenses_.",
     link: {
-      text: "linktext",
-      url: "https://example.com"
+      text: "Go to website",
+      url: "https://mage.tools"
     },
-    skills: ["a", "b", "c"],
+    skills: [
+      "Jekyll",
+      "Ruby",
+      "JavaScript",
+      "UI",
+      "UX",
+      "Branding",
+      "Accessibility",
+      "Performance",
+      "Responsive design",
+      "Copywriting"
+    ],
     images: [
       {
-        url: "https://example.com/cat.png",
+        url: "/assets/1.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/2.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/3.png",
+        description: "imagedescription"
+      },
+      {
+        url: "/assets/4.png",
         description: "imagedescription"
       }
     ]
