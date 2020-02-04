0a178e8ea7263c247427b6835ff0a57858fbbf4a
Mark Thomas Miller <mark@mayvalellc.com>
2020-02-03 20:55:33 -0500

    Format skills as a comma-separated list

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import styles from "./Skills.module.css";
 
 const Skills = ({ skills }) => (
-  <ul className={styles.skills}>
-    {skills.map((skill, i) => (
-      <li key={`skill${i}`}>{skill}</li>
-    ))}
-  </ul>
+  <div className={styles.skills}>
+    <strong>What I handled:</strong>
+    <ul>
+      {skills.map((skill, i) => (
+        <li className={styles.skill} key={`skill${i}`}>
+          {skill}
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default Skills;
